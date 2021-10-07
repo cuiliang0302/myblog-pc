e9153ad0b0d6c391f8403dbcfd85f19140a5dcd3
cuiliang <1554382111@qq.com>
2021-10-07 17:53:57 +0800

    更新vite打包相关配置

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -28,5 +28,16 @@ export default defineConfig(({mode}) => {
 		optimizeDeps: {
 			include: ['@kangc/v-md-editor/lib/theme/vuepress.js'],
 		},
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes('node_modules')) {
+							return id.toString().split('node_modules/')[1].split('/')[0].toString();
+						}
+					}
+				}
+			}
+		}
 	}
 })
